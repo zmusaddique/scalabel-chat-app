642c622eb6fab927f909e492c2df666876777e2b
zmusaddique <musaddique092@gmail.com>
2024-07-01 15:34:14 +0530

    socket backend

---
+++ apps/server/src/index.ts
@@ -0,0 +1,17 @@
+import http from 'http'
+import SocketService from './services/socket';
+
+async function init() {
+    const socketService = new SocketService();
+
+    const httpServer = http.createServer();
+    const PORT = process.env.PORT ? process.env.PORT : 8000
+
+    socketService.io.attach(httpServer);
+
+    httpServer.listen(PORT, () => console.log(`HTTP server created at PORT: ${PORT}`));
+
+    socketService.initListeners();
+}
+
+init();
---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,29 @@
+import {Server} from "socket.io"
+
+class SocketService {
+    private _io: Server;
+
+    constructor() {
+        console.log("Init socket service...");
+        this._io = new Server();
+    }
+
+    public initListeners() {
+        const io = this.io;
+        console.log("Init socket listeners...");
+        
+        io.on('connect', socket => {
+            console.log("New socket connected ", socket.id);
+
+            socket.on('event:message', async ({message}: {message: string}) => {
+                console.log('New message received: ', message)
+            })
+        })
+    }
+
+    get io() {
+        return this._io;
+    }
+}
+
+export default SocketService;
